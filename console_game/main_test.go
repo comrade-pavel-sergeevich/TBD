141ace62a123899e827c32e2f247421e53981fc3
comrade-pavel-sergeevich <90038306+comrade-pavel-sergeevich@users.noreply.github.com>
2023-01-10 07:20:46 +0300

    РПБД console_game Лебедев 3281
    переделал как нужно

--- console_game/main_test.go
+++ console_game/main_test.go
@@ -11,17 +11,26 @@ func TestString(t *testing.T) {
 	require.True(t, "D=10, Z=100, U=20, V=30" == krot.String())
 }
 func TestDig(t *testing.T) {
-	dig(true)
-	dig(false)
+	krot := Newmole()
+	krot.dig(true)
+	krot.dig(false)
 }
 func TestSmokeweed(t *testing.T) {
-	smokeweed(true)
-	smokeweed(false)
+	krot := Newmole()
+	krot.smokeweed(true)
+	krot.smokeweed(false)
+	krot = mole{100, 100, 100, 100}
+	krot.smokeweed(false)
 }
 func TestShvatka(t *testing.T) {
-	gopstop(0)
-	gopstop(1)
-	gopstop(2)
+	krot := mole{1, 1, 1, 1}
+	krot.gopstop(0)
+	krot.gopstop(1)
+	krot.gopstop(2)
+	krot = mole{1000, 1000, 1000, 1000}
+	krot.gopstop(0)
+	krot.gopstop(1)
+	krot.gopstop(2)
 }
 func TestMoleChanges(t *testing.T) {
 	krot.changeD(1)
@@ -30,15 +39,24 @@ func TestMoleChanges(t *testing.T) {
 	krot.changeV(1)
 }
 func TestSleep(t *testing.T) {
-	night()
-	daysleep()
+	krot := Newmole()
+	krot.night()
+	krot.daysleep()
 }
 func TestCheck(t *testing.T) {
 	krot := mole{0, 0, 0, 0}
-	check()
+	krot.check()
 	require.Equal(t, false, krot.isAlive())
+	krot = mole{10, 10, 10, 10}
+	krot.check()
+	require.Equal(t, true, krot.isAlive())
+	krot = mole{1000, 1000, 1000, 1000}
+	krot.check()
+	require.Equal(t, true, krot.isAlive())
 }
 func TestRespect(t *testing.T) {
 	krot := mole{0, 0, 0, 0}
 	require.Equal(t, false, krot.isRespectable())
+	krot = mole{1000, 1000, 1000, 1000}
+	require.Equal(t, true, krot.isRespectable())
 }
